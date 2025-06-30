61c54119716d17041dd4a63b8c5806fd494ad7f1
Valentin D. Pinkman <valentin.dealmeida@ledger.fr>
2025-06-30 15:33:41 +0200

    ✨ (core): ConnectDevice use case

--- packages/ledger-button-core/src/internal/device/service/DeviceManagementKitService.ts
+++ packages/ledger-button-core/src/internal/device/service/DeviceManagementKitService.ts
@@ -2,6 +2,7 @@ import {
   ConsoleLogger,
   DeviceManagementKit,
   DeviceManagementKitBuilder,
+  DeviceSessionId,
   TransportIdentifier,
 } from "@ledgerhq/device-management-kit";
 import {
@@ -12,21 +13,28 @@ import {
   webHidIdentifier,
   webHidTransportFactory,
 } from "@ledgerhq/device-transport-kit-web-hid";
-import { inject, injectable } from "inversify";
+import { type Factory, inject, injectable } from "inversify";
 
 import { type DeviceModuleOptions } from "../../diTypes.js";
+import { loggerModuleTypes } from "../../logger/loggerModuleTypes.js";
+import { type LoggerPublisher } from "../../logger/service/LoggerPublisher.js";
 import { deviceModuleTypes } from "../deviceModuleTypes.js";
 
 @injectable()
 export class DeviceManagementKitService {
-  private _dmk: DeviceManagementKit;
-  public usbIdentifier: TransportIdentifier = webHidIdentifier;
+  private readonly logger: LoggerPublisher;
+  private readonly _dmk: DeviceManagementKit;
+  public hidIdentifier: TransportIdentifier = webHidIdentifier;
   public bleIdentifier: TransportIdentifier = webBleIdentifier;
+  private _currentSessionId?: string;
 
   constructor(
+    @inject(loggerModuleTypes.LoggerPublisher)
+    loggerFactory: Factory<LoggerPublisher>,
     @inject(deviceModuleTypes.DmkConfig)
     args: DeviceModuleOptions,
   ) {
+    this.logger = loggerFactory("DeviceManagementKitService");
     const builder = new DeviceManagementKitBuilder();
 
     builder
@@ -40,5 +48,56 @@ export class DeviceManagementKitService {
 
   get dmk() {
     return this._dmk;
+  }
+
+  get sessionId() {
+    return this._currentSessionId;
+  }
+
+  async connectToDevice({ type }: { type: "hid" | "ble" }) {
+    const identifier = type === "hid" ? this.hidIdentifier : this.bleIdentifier;
+    this.logger.debug(`Connecting to device`, { identifier });
+
+    const dmk = this.dmk;
+    return new Promise<DeviceSessionId>((resolve, reject) => {
+      this.logger.debug(`Starting discovery`, { identifier });
+      dmk.startDiscovering({ transport: identifier }).subscribe({
+        next: (device) => {
+          this.logger.debug(`Found device`, { device });
+          dmk
+            .connect({ device })
+            .then((sessionId) => {
+              this.logger.debug(`Connected to device`, { sessionId });
+              this._currentSessionId = sessionId;
+              resolve(sessionId);
+            })
+            .catch((error) => {
+              this.logger.error(`Failed to connect to device`, { error });
+              reject(error);
+            })
+            .finally(async () => {
+              await dmk.stopDiscovering();
+            });
+        },
+        error: async (error) => {
+          this.logger.error(`Failed to start discovery`, { error });
+          await dmk.stopDiscovering();
+          reject(error);
+        },
+      });
+    });
+  }
+
+  async disconnectFromDevice() {
+    if (!this._currentSessionId) {
+      return;
+    }
+    try {
+      await this.dmk.disconnect({ sessionId: this._currentSessionId });
+      this._currentSessionId = undefined;
+    } catch (error) {
+      this.logger.error(`Failed to disconnect from device`, { error });
+      throw error;
+    }
   }
 }
--- packages/ledger-button-core/src/internal/device/use-case/ConnectDevice.ts
+++ packages/ledger-button-core/src/internal/device/use-case/ConnectDevice.ts
@@ -1,36 +1,32 @@
-import { inject, injectable } from "inversify";
+import { type DeviceSessionId } from "@ledgerhq/device-management-kit";
+import { type Factory, inject, injectable } from "inversify";
 
+import { loggerModuleTypes } from "../../logger/loggerModuleTypes.js";
+import { LoggerPublisher } from "../../logger/service/LoggerPublisher.js";
 import { deviceModuleTypes } from "../deviceModuleTypes.js";
+import { DeviceConnectionError } from "../model/errors.js";
 import { DeviceManagementKitService } from "../service/DeviceManagementKitService.js";
 
 @injectable()
 export class ConnectDevice {
+  private readonly logger: LoggerPublisher;
   constructor(
+    @inject(loggerModuleTypes.LoggerPublisher)
+    loggerFactory: Factory<LoggerPublisher>,
     @inject(deviceModuleTypes.DeviceManagementKitService)
     private readonly deviceManagementKitService: DeviceManagementKitService,
-  ) {}
+  ) {
+    this.logger = loggerFactory("ConnectDevice UseCase");
+  }
 
-  async execute({ type }: { type: "usb" | "ble" }): Promise<string> {
-    const identifier =
-      type === "usb"
-        ? this.deviceManagementKitService.usbIdentifier
-        : this.deviceManagementKitService.bleIdentifier;
-
-    const dmk = this.deviceManagementKitService.dmk;
-    return new Promise((resolve, reject) => {
-      dmk.startDiscovering({ transport: identifier }).subscribe({
-        next: (device) => {
-          dmk
-            .connect({ device })
-            .then((sessionId) => {
-              resolve(sessionId);
-            })
-            .catch((error) => {
-              console.error(error);
-              reject(error);
-            });
-        },
+  async execute({ type }: { type: "hid" | "ble" }): Promise<DeviceSessionId> {
+    return this.deviceManagementKitService
+      .connectToDevice({ type })
+      .catch((error) => {
+        this.logger.error(`Failed to connect to device`, { error });
+        throw new DeviceConnectionError(`Failed to connect to device`, {
+          error,
+        });
       });
-    });
   }
 }
