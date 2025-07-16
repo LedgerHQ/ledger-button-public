0defa6fde8cb8136de3d8b24c05601e8ba29cb5e
Gustavo Porto <gus@p8s.co>
2025-07-16 19:02:03 -0300

    💄 (modal): Update modal UI

--- packages/ledger-button-ui/src/lib/components/atom/modal/ledger-modal.ts
+++ packages/ledger-button-ui/src/lib/components/atom/modal/ledger-modal.ts
@@ -1,6 +1,6 @@
 import "../../molecule/toolbar/ledger-toolbar";
 
-import { html, LitElement, unsafeCSS } from "lit";
+import { css, html, LitElement, PropertyValues, unsafeCSS } from "lit";
 import { customElement, property, query } from "lit/decorators.js";
 
 import tailwindStyles from "../../../../styles.css?inline";
@@ -12,10 +12,17 @@ export interface LedgerModalAttributes {
   closeOnEscape?: boolean;
 }
 
+type CustomEventData = {
+  timestamp: number;
+};
+
 @customElement("ledger-modal")
 export class LedgerModal extends LitElement {
   @property({ type: Boolean, attribute: "is-open", reflect: true })
   isOpen = false;
+
+  @property({ type: Boolean, attribute: false })
+  isClosing = false;
 
   @property({ type: String })
   override title = "";
@@ -29,7 +36,37 @@ export class LedgerModal extends LitElement {
   private focusableElements: HTMLElement[] = [];
   private previousBodyOverflow = "";
 
-  static override styles = [unsafeCSS(tailwindStyles)];
+  static override styles = [
+    unsafeCSS(tailwindStyles),
+    css`
+      :host {
+        display: none;
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        top: 0;
+        left: 0;
+        z-index: 1000;
+      }
+      :host([isOpen]) {
+        display: flex;
+      }
+      .modal-overlay {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
+        height: 100%;
+        position: relative;
+        z-index: 1;
+        opacity: 0;
+      }
+    `,
+  ];
+
+  override firstUpdated(changedProperties: PropertyValues) {
+    super.firstUpdated(changedProperties);
+  }
 
   override connectedCallback() {
     super.connectedCallback();
@@ -41,25 +78,54 @@ export class LedgerModal extends LitElement {
     this.cleanupEventListeners();
   }
 
-  override updated(changedProperties: Map<string, unknown>) {
+  override updated(changedProperties: Map) {
     super.updated(changedProperties);
 
     if (changedProperties.has("isOpen")) {
-      if (this.isOpen) {
-        this.handleOpen();
+      if (this.isClosing) {
         return;
       }
 
-      this.handleClose();
+      if (this.isOpen) {
+        this.dispatchEvent(
+          new CustomEvent<CustomEventData>("modal-opened", {
+            bubbles: true,
+            composed: true,
+            detail: {
+              timestamp: Date.now(),
+            },
+          }),
+        );
+        return;
+      }
+    }
+
+    if (changedProperties.has("isClosing")) {
+      if (this.isClosing) {
+        this.dispatchEvent(
+          new CustomEvent<CustomEventData>("modal-closed", {
+            bubbles: true,
+            composed: true,
+            detail: {
+              timestamp: Date.now(),
+            },
+          }),
+        );
+        return;
+      }
     }
   }
 
   private setupEventListeners() {
     document.addEventListener("keydown", this.handleKeydown);
+    this.addEventListener("modal-opened", this.handleOpen);
+    this.addEventListener("modal-closed", this.handleClose);
   }
 
   private cleanupEventListeners() {
     document.removeEventListener("keydown", this.handleKeydown);
+    this.removeEventListener("modal-opened", this.handleOpen);
+    this.removeEventListener("modal-closed", this.handleClose);
   }
 
   private handleKeydown = (event: KeyboardEvent) => {
@@ -92,8 +158,6 @@ export class LedgerModal extends LitElement {
   }
 
   private handleClose() {
-    document.body.style.overflow = this.previousBodyOverflow;
-
     dispatchEvent(
       new CustomEvent("model-closed", {
         bubbles: true,
@@ -126,6 +190,7 @@ export class LedgerModal extends LitElement {
   };
 
   private handleToolbarClose = () => {
+    document.body.style.overflow = this.previousBodyOverflow;
     this.closeModal();
   };
 
@@ -138,13 +203,9 @@ export class LedgerModal extends LitElement {
   }
 
   override render() {
-    if (!this.isOpen) {
-      return html``;
-    }
-
     return html`
       <div
-        class="modal-overlay relative z-10"
+        class="modal-overlay w-screen h-screen relative z-10 justify-center bg-canvas-overlay"
         @click=${this.handleOverlayClick}
         role="dialog"
         aria-modal="true"
@@ -153,11 +214,7 @@ export class LedgerModal extends LitElement {
         data-testid="modal-overlay"
       >
         <div
-          class="bg-background-canvas-overlay fixed inset-0"
-          aria-hidden="true"
-        ></div>
-        <div
-          class="w-screen w-400 h-448 fixed inset-0 z-10 flex flex-col overflow-y-auto rounded-xl bg-black"
+          class="w-screen h-448 fixed inset-0 z-10 flex w-384 flex-col self-center justify-self-center overflow-y-auto rounded-xl bg-black"
           @click=${(e: Event) => e.stopPropagation()}
         >
           <ledger-toolbar
