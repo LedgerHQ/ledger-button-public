6ed41670d8b0a81a4db7019e46321b008c0a781c
Gustavo Porto <gus@p8s.co>
2025-09-21 21:34:02 -0300

    ♻️ (api): Reorder error exports for correct resoltion

--- packages/ledger-button-core/src/api/errors/index.ts
+++ packages/ledger-button-core/src/api/errors/index.ts
@@ -1,4 +1,4 @@
-export * from "../../internal/storage/model/errors.js";
 export * from "../../internal/ledgersync/model/errors.js";
+export * from "../../internal/storage/model/errors.js";
 export * from "./DeviceErrors.js";
 export * from "./LedgerButtonError.js";
