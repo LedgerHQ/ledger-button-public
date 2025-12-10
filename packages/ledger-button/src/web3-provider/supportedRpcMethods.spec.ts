51c250e6d033f2969379afc0d85155898a1ec3e4
pdeville-ledger <pierre-alexis.deville@ledger.fr>
2025-12-10 17:18:48 +0100

    ♻️ (rpc) [LBD-214]: Separate locally handled and broadcasted RPC methods
    - Split SUPPORTED_RPC_METHODS into LOCALLY_HANDLED_RPC_METHODS and BROADCASTED_TO_NODE_RPC_METHODS
    - Add comments clarifying which methods are handled locally vs broadcasted to Node RPC
    - Update test file to use new SUPPORTED_RPC_METHODS constant

--- packages/ledger-button/src/web3-provider/supportedRpcMethods.spec.ts
+++ packages/ledger-button/src/web3-provider/supportedRpcMethods.spec.ts
@@ -2,12 +2,12 @@ import { describe, expect, test } from "vitest";
 
 import {
   isSupportedRpcMethod,
-  STANDARD_JSON_RPC_METHODS,
+  SUPPORTED_RPC_METHODS,
 } from "./supportedRpcMethods.js";
 
 describe("supportedRpcMethods", () => {
   describe("isSupportedRpcMethod", () => {
-    test.each([...STANDARD_JSON_RPC_METHODS])(
+    test.each([...SUPPORTED_RPC_METHODS])(
       "should return true for supported method: %s",
       (method) => {
         expect(isSupportedRpcMethod(method)).toBe(true);
