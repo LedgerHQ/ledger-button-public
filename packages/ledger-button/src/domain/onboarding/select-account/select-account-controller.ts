7ae35a7979897c51b932266b5119c3a9045d4de7
Tom Seropian <tom.seropian-ext@ledger.fr>
2026-02-04 13:56:51 +0000

    :zap: (ledger-button) [LBD-279]: Add progressive loading state to SelectAccountController

--- packages/ledger-button/src/domain/onboarding/select-account/select-account-controller.ts
+++ packages/ledger-button/src/domain/onboarding/select-account/select-account-controller.ts
@@ -16,7 +16,6 @@ export class SelectAccountController implements ReactiveController {
   isAccountsLoading = false;
   balanceLoadingStates = new Map<string, BalanceLoadingState>();
   private accountsSubscription?: Subscription;
-  private initialAccountsReceived = false;
 
   constructor(
     private readonly host: ReactiveControllerHost,
@@ -27,96 +26,40 @@ export class SelectAccountController implements ReactiveController {
   }
 
   hostConnected() {
-    this.subscribeToAccounts();
+    this.getAccounts();
   }
 
   hostDisconnected() {
     if (this.accountsSubscription) {
       this.accountsSubscription.unsubscribe();
-      this.accountsSubscription = undefined;
     }
-    this.initialAccountsReceived = false;
   }
 
-  private subscribeToAccounts() {
+  async getAccounts() {
     this.isAccountsLoading = true;
     this.host.requestUpdate();
 
-    this.accountsSubscription = this.core.getAccountsWithBalance().subscribe({
-      next: (accounts) => {
-        this.updateAccounts(accounts);
-      },
-      error: (error) => {
-        this.isAccountsLoading = false;
-        this.host.requestUpdate();
-        throw error;
-      },
-    });
-  }
-
-  private updateAccounts(accounts: Account[]) {
-    const previousAccounts = this.createPreviousAccountsMap();
-
-    this.accounts = accounts ?? [];
-    this.isAccountsLoading = false;
-
-    if (!this.initialAccountsReceived) {
-      this.initializeAccountsLoadingStates(this.accounts);
-    } else {
-      this.processAccountUpdates(this.accounts, previousAccounts);
-    }
-
-    this.host.requestUpdate();
-  }
-
-  private createPreviousAccountsMap(): Map<string, Account> {
-    return new Map(this.accounts.map((acc) => [acc.id, acc]));
-  }
-
-  private initializeAccountsLoadingStates(accounts: Account[]): void {
-    this.initialAccountsReceived = true;
-    for (const account of accounts) {
-      this.balanceLoadingStates.set(account.id, "loading");
+    try {
+      const accounts = await this.core.fetchAccounts();
+      this.accounts = accounts ?? [];
+      this.isAccountsLoading = false;
+      this.initializeBalanceLoadingStates();
+    } catch (error) {
+      this.isAccountsLoading = false;
+      this.host.requestUpdate();
+      throw error;
     }
   }
 
-  private processAccountUpdates(
-    accounts: Account[],
-    previousAccounts: Map<string, Account>,
-  ): void {
-    for (const account of accounts) {
-      const previousAccount = previousAccounts.get(account.id);
-      this.updateAccountBalanceState(account, previousAccount);
-    }
-  }
-
-  private updateAccountBalanceState(
-    account: Account,
-    previousAccount: Account | undefined,
-  ): void {
-    if (account.balance !== undefined) {
-      this.balanceLoadingStates.set(account.id, "loaded");
-    } else if (previousAccount) {
-      if (this.shouldMarkAsError(account, previousAccount)) {
-        this.balanceLoadingStates.set(account.id, "error");
+  private initializeBalanceLoadingStates() {
+    for (const account of this.accounts) {
+      if (account.balance !== undefined) {
+        this.balanceLoadingStates.set(account.id, "loaded");
+      } else {
+        this.balanceLoadingStates.set(account.id, "loading");
       }
-    } else {
-      this.balanceLoadingStates.set(account.id, "loading");
     }
-  }
-
-  private shouldMarkAsError(
-    account: Account,
-    previousAccount: Account,
-  ): boolean {
-    const previousBalance = previousAccount.balance;
-    const currentState = this.balanceLoadingStates.get(account.id);
-
-    return (
-      previousBalance === undefined &&
-      account.balance === undefined &&
-      currentState === "loading"
-    );
+    this.host.requestUpdate();
   }
 
   setBalanceLoadingState(accountId: string, state: BalanceLoadingState): void {
@@ -150,9 +93,9 @@ export class SelectAccountController implements ReactiveController {
   handleAccountItemClick = (
     event: CustomEvent<AccountItemClickEventDetail>,
   ) => {
-    const account = this.accounts.find(
-      (acc) => acc.id === event.detail.ledgerId,
-    );
+    const account = this.core
+      .getAccounts()
+      .find((acc) => acc.id === event.detail.ledgerId);
 
     if (account) {
       this.selectAccount(account);
@@ -175,9 +118,9 @@ export class SelectAccountController implements ReactiveController {
   handleAccountItemShowTokensClick = (
     event: CustomEvent<AccountItemClickEventDetail>,
   ) => {
-    const account = this.accounts.find(
-      (acc) => acc.id === event.detail.ledgerId,
-    );
+    const account = this.core
+      .getAccounts()
+      .find((acc) => acc.id === event.detail.ledgerId);
 
     if (account) {
       this.core.setPendingAccountId(account.id);
