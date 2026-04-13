7a67df802d9354a6838645892870c48e320d0f51
pdeville-ledger <pierre-alexis.deville@ledger.fr>
2026-04-13 16:12:30 +0200

    ✨ (ledger-button): Refactor badge count handling in floating button components

--- packages/ledger-button/src/components/atom/floating-button/floating-button-badge-animation-controller.ts
+++ packages/ledger-button/src/components/atom/floating-button/floating-button-badge-animation-controller.ts
@@ -16,7 +16,6 @@ export class BadgeAnimationController implements ReactiveController {
   animatedVariant: BadgeVariant | null = null;
   delayTooltipOpen = false;
   badgeAppearing = false;
-  frozenCount: number | null = null;
 
   private _isAnimating = false;
   private _hasPending = false;
@@ -72,7 +71,6 @@ export class BadgeAnimationController implements ReactiveController {
     hasPending: boolean,
     postCloseTooltip: boolean,
     modalIsOpen: boolean,
-    pendingCount: number,
   ): void {
     this._celebration = celebration;
     this._hasPending = hasPending;
@@ -88,7 +86,6 @@ export class BadgeAnimationController implements ReactiveController {
 
     if (celebrationChanged) {
       if (celebration) {
-        this.frozenCount = pendingCount;
         this._pendingAction = "transition-validated";
       } else if (hasPending) {
         this._pendingAction = "transition-pending";
@@ -188,7 +185,6 @@ export class BadgeAnimationController implements ReactiveController {
     const shrinkEl = this.queryBadge();
     if (shrinkEl) await this.animation.shrinkOut(shrinkEl);
 
-    this.frozenCount = null;
     this.animatedVariant = to;
     this.host.requestUpdate();
     await this.host.updateComplete;
