46f9a9a13f3ca7412111483de42af7f0211a7671
Pierre Vautherin <pierre.vautherin@ledger.fr>
2026-04-24 16:21:50 +0200

    🎨 (icons): Align file names

--- packages/ledger-button/src/components/atom/icon/languageIcon.ts
+++ packages/ledger-button/src/components/atom/icon/languageIcon.ts
@@ -1,6 +1,6 @@
 import { html } from "lit";
 
-export const LanguageIcon = html`
+export default html`
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width="24"
@@ -17,5 +17,3 @@ export const LanguageIcon = html`
     />
   </svg>
 `;
-
-export default LanguageIcon;
