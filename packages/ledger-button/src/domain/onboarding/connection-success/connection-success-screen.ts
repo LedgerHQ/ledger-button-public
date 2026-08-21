801b21de9ca9edcd4c74f62670bae2cf595c128a
Pierre Vautherin <pierre.vautherin@ledger.fr>
2026-08-21 16:42:13 +0200

    ✨ (ledger-button) [LBD-741]: Update toolbar

--- packages/ledger-button/src/domain/onboarding/connection-success/connection-success-screen.ts
+++ packages/ledger-button/src/domain/onboarding/connection-success/connection-success-screen.ts
@@ -53,20 +53,10 @@ export class ConnectionSuccessScreen extends LitElement {
     const translations = this.languages.currentTranslation;
 
     return html`
-      <div class="relative flex h-full flex-col gap-32 p-24">
-        <div class="absolute top-16 right-16 z-10">
-          <ledger-button
-            .icon=${true}
-            variant="noBackground"
-            iconType="close"
-            size="xs"
-            .label=${translations.onboarding.connectionSuccess.close}
-            @click=${this.handleClose}
-          ></ledger-button>
-        </div>
+      <div class="flex h-full flex-col gap-32 p-24">
         <div class="flex flex-1 flex-col items-center justify-center gap-24">
           <div
-            class="bg-muted-transparent text-success flex h-72 w-72 items-center justify-center rounded-full"
+            class="bg-muted-transparent text-success flex h-72 w-72 items-center justify-center rounded-full p-12"
           >
             <ledger-icon
               type="checkMarkCircleFill"
@@ -74,7 +64,7 @@ export class ConnectionSuccessScreen extends LitElement {
               fillColor="currentColor"
             ></ledger-icon>
           </div>
-          <div class="flex flex-col gap-8 text-center">
+          <div class="flex w-full flex-col gap-8 text-center">
             <h2 class="heading-3-semi-bold text-base">
               ${translations.onboarding.connectionSuccess.title}
             </h2>
